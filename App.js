02ea1d076e7c0a5610ab23b55e1e3ba3379ca5c6
bishal42 <sthabishal422@gmail.com>
2022-12-29 08:48:27 +0100

    fetching the data from api

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import React, { useState, useEffect } from "react";
 
 export default function App() {
+  const [cat, setcat] = useState([]);
+  const API_URL = "https://catfact.ninja/fact";
+
+  const getcats = async () => {
+    try {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      setcat(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    getcats();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground
+        style={styles.image}
+        source={require("./assets/cat.jpg")}
+      >
+        <View style={styles.textContainer}>
+          <Text style={styles.header}>Randon Cat fact</Text>
+          <Text style={styles.fact}>{cat.fact}</Text>
+          <Text style={styles.length}>Length:{cat.length}</Text>
+          <StatusBar style="auto" />
+        </View>
+      </ImageBackground>
     </View>
   );
 }
@@ -13,8 +39,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  header: {
+    fontWeight: "bold",
+    fontSize: 26,
+    color: "#6E654A",
+  },
+  fact: {
+    fontSize: 19,
+  },
+  length: {},
+  image: {
+    width: "98%",
+    height: "98%",
+  },
+  textContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
